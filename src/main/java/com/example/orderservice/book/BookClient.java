72a8c17d364c07d7197fad4c9326696e37471a8f
realkoder <alex8970@stud.kea.dk>
2024-01-18 12:09:18 +0100

    Added resilience strategy for BookClient with retry and resume for errors.

--- src/main/java/com/example/orderservice/book/BookClient.java
+++ src/main/java/com/example/orderservice/book/BookClient.java
@@ -3,7 +3,11 @@ package com.example.orderservice.book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
@@ -16,6 +20,11 @@ public class BookClient {
                 .get()
                 .uri(BOOKS_ROOT_API + isbn)
                 .retrieve()
-                .bodyToMono(Book.class);
+                .bodyToMono(Book.class)
+                .timeout(Duration.ofSeconds(3), Mono.empty())
+                .onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
+                // Three retries are allowed with 100 ms initial backoff - exponential backoff strategy
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+                .onErrorResume(Exception.class, exception -> Mono.empty());
     }
 }
