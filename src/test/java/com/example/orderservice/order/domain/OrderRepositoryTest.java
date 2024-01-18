85ac41e31eb8f8501885c5c3e8e5b397ac41995e
realkoder <alex8970@stud.kea.dk>
2024-01-18 12:48:23 +0100

    Implemented test cases for BookClientTest and Orderrepository relying on @Testcontainers

---
+++ src/test/java/com/example/orderservice/order/domain/OrderRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.example.orderservice.order.domain;
+
+import com.example.orderservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataR2dbcTest
+@Import(DataConfig.class)
+@Testcontainers
+class OrderRepositoryTest {
+    @Container
+    static PostgreSQLContainer<?> postgresql =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderRepositoryTest::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s",
+                postgresql.getHost(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgresql.getDatabaseName());
+    }
+
+    @Test
+    void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder("1234567890", 3);
+        StepVerifier
+                .create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(
+                        order -> order.status().equals(OrderStatus.REJECTED) &&
+                                order.id() > 0 &&
+                                order.createdDate() != null &&
+                                order.lastModifiedDate() != null &&
+                                order.version() > 0
+                ).verifyComplete();
+    }
+}
